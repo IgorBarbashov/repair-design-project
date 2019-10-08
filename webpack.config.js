46089aaf819da254b847b30280b1353f0338c3aa
Igor Barbashov <bis200705@gmail.com>
2019-10-08 17:55:53 +0300

    feat: add header-desktop

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,18 @@ module.exports = {
           fallback: "style-loader",
           use: ["css-loader", "sass-loader"]
         })
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
       }
     ]
   },
